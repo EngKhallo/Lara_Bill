c2f60759cc10944772a9a48fa5d93a1468c60aa3
EngKhallo <engkhalid.ict@gmail.com>
2023-09-20 01:51:27 +0300

    Invoice Filter class

---
+++ app/Filters/InvoicesFilter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filters;
+
+use Illuminate\Http\Request;
+
+
+class InvoicesFilter extends ApiFilter{
+    protected $allowedParams = [
+        'customerId' => ['eq'],
+        'amount' => ['eq', 'lt', 'lte', 'gt', 'gte'],
+        'status' => ['eq', 'ne'],
+        'billedDate' => ['eq', 'lt', 'lte', 'gt', 'gte'],
+        'paidDate' => ['eq', 'lt', 'lte', 'gt', 'gte'],
+    ];
+
+    protected $columnMap = [
+        'customerId' => 'customer_id',
+        'billedDate' => 'billed_date',
+        'paidDate' => 'paid_date',
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>=',
+        'ne' => '!='
+    ];
+
+}
