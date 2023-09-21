e0da5e426cbefc42e94c1518b4b054e1cb69965d
EngKhallo <engkhalid.ict@gmail.com>
2023-09-21 18:44:17 +0300

    Rf

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -29,14 +29,6 @@ class InvoiceController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreInvoiceRequest $request)
