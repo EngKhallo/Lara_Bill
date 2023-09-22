ede15677b05f1cd9643202b85f0c4d675787c8e0
EngKhallo <engkhalid.ict@gmail.com>
2023-09-22 21:28:36 +0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 //Protected routes
 Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function () {
     Route::apiResource('customers', CustomerController::class)->except(['index', 'show']);
